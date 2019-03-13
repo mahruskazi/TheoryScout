68b36b61069ab9501352cc0582fe62b10bf11fa9
Mahrus Kazi <mkazi@tesla.com>
2019-03-12 19:06:09 -0700

    Able to compile firebase

--- src/components/DataScreen.js
+++ src/components/DataScreen.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import 'babel-polyfill';
+import firebase from "firebase";
+
+var config = {
+  databaseURL: "https://theory-scout.firebaseio.com",
+  projectId: "theory-scout"
+}
 
 export default class DataScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
+  }
+
+  componentWillMount() {
+    firebase.initializeApp(config);
   }
 
   render() {
